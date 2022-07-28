a4b733803c8e337d1c8520141a5fd4a52998ce74
fefilov_a <fefilov.toha1999@yandex.ru>
2022-07-28 21:06:08 +0300

    fixed spinner bug

--- src/pages/mainPage/MainPageContainer.jsx
+++ src/pages/mainPage/MainPageContainer.jsx
@@ -4,7 +4,7 @@ import Spinner from "./Spinner";
 export const MainPageContainer = ({ stateLoading, stateError, fetchedArray, fetchNewCats, moreCatsLoading }) => {
     return (
         <>
-            {stateLoading && <Spinner />}
+            {/* {stateLoading && <Spinner />} */}
             {stateError && <p className="text-center text-lg text-red-600">{stateError}</p>}
             <div
                 className="grid 2xl:grid-cols-5 2xl:mt-[48px] 2xl:gap-y-[52px] xl:px-[62px] lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-3 sm:mt-[24px] sm:px-[32px] sm:gap-y-[20px] md:gap-y-[40px]
@@ -14,7 +14,7 @@ export const MainPageContainer = ({ stateLoading, stateError, fetchedArray, fetc
                     <Card key={cat.id} cat={cat} i={i} />
                 ))}
             </div>
-            {moreCatsLoading ? (
+            {moreCatsLoading || stateLoading ? (
                 <Spinner />
             ) : (
                 <div className="text-center text-lg cursor-pointer mb-[31px] mt-[48px]" onClick={fetchNewCats}>
